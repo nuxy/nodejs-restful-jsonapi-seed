cfced448d4375e7c6bb189f575c0dfb5d1a1589d
Marc S. Brooks <marc@mbrooks.info>
2018-04-02 14:45:54 -0700

    Removed public files and views support (this is NOT a webserver)

--- src/app.js
+++ src/app.js
@@ -9,15 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
