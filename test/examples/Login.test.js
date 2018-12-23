128c9e92a656b27200065d4cc5c9352bef965b95
Marc S. Brooks <marc@mbrooks.info>
2018-12-23 11:25:11 -0800

    Relocated timeout property to global scope

--- test/examples/Login.test.js
+++ test/examples/Login.test.js
@@ -7,6 +7,7 @@ import shared from 'mocha-shared';
  * Login service integration test.
  */
 describe('Login', function() {
+  this.timeout(1000);
 
   /**
    * Include shared behaviors.
@@ -30,8 +31,6 @@ describe('Login', function() {
 
   describe('Check session exists', function() {
     it('returns no errors', function(done) {
-      this.timeout(1000);
-
       this.request.get('/login')
         .end(function(err, res) {
           chai.expect(res.statusCode) .to.equal(200);
